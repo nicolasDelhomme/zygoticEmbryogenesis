71e69a2edfb427ea7f0a812a0ca97ba6a1e0c546
Michael Stewart <mstewart@aspseq.fysbot.umu.se>
2019-09-05 14:25:37 +0200

    Copied and Modified submitMultiQC.sh - now called submitMultiQConlyraw.sh, using only fastqc/raw data as an input.
    Outputing to ./RNA-Seq/multiqc/onlyraw

---
+++ pipeline/submitMultiQCronlyraw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#set -ex
+
+proj=u2019016
+mail=mist0069@student.umu.se
+in=/mnt/picea/projects/spruce/uegertsdotter/ZE-developmental-series/RNA-Seq/fastqc/raw
+out=/mnt/picea/projects/spruce/uegertsdotter/ZE-developmental-series/RNA-Seq/multiqc/onlyraw
+
+if [ ! -d $out ]; then
+	mkdir -p $out
+fi
+
+module load bioinfo-tools multiqc
+
+sbatch --mail-user=$mail -o $in/multiqc.out -e $in/multiqc.err -A $proj ../UPSCb-common/pipeline/runMultiQC.sh $in $out
