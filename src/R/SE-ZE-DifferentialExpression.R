61f6478b731a9a3fe5d16b65515dc23e5288cb77
Nicolas <nicolas.delhomme@umu.se>
2020-12-18 03:30:40 +0100

    Added a list of GOI. Revisited the batch correction. Performed the DE analysis.

---
+++ src/R/SE-ZE-DifferentialExpression.R
@@ -0,0 +1,154 @@
+#' ---
+#' title: "ZE and SE differential expression analysis"
+#' author: "Nicolas Delhomme && Michael Stewart"
+#' date: "`r Sys.Date()`"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    number_sections: true
+#'    code_folding: hide
+#' ---
+#' # Setup
+#' * Libraries
+suppressPackageStartupMessages({
+  library(DESeq2)
+  library(dplyr)
+  library(gplots)
+  library(ggplot2)
+  library(here)
+  library(hyperSpec)
+  library(limma)
+  library(readr)
+  library(tibble)
+  library(RColorBrewer)
+  library(VennDiagram)
+})
+
+#' * Helpers
+source(here("UPSCb-common/src/R/gopher.R"))
+
+#' * Graphics
+hpal <- colorRampPalette(c("blue","white","red"))(100)
+pal <- brewer.pal(8,"Dark2")
+
+#' * Data
+suppressPackageStartupMessages(load(here("analysis/batchCorrection/vsd.rda")))
+
+#' * Time points of interest
+sample.sel <- (vsd$Time %in% paste0("B",3:6) & vsd$Tissue =="SE") | 
+(vsd$Tissue!="SE" & vsd$Time %in% paste0("B",4:9))
+vsd <- vsd[,sample.sel]
+vsd$Time <- droplevels(vsd$Time)
+
+#' * Devise a pseudotime from the PCA after bacth correction
+# The order is B4-5 ZE; B3 SE; B6 ZE, B4-5 SE, B7-9 ZE, B6 SE
+# corresponding to pseudo-time 1-2,3,4,5-6,7-9,10
+vsd$PseudoTime <- as.integer(vsd$Time) -1 + 
+  ifelse(vsd$Tissue=="SE",
+         ifelse(vsd$Time=="B3",3,
+                ifelse(vsd$Time %in% c("B4","B5"),4,7)),
+         ifelse(vsd$Time=="B6",1,
+           ifelse(vsd$Time %in% paste0("B",7:9),3,0)))
+#' ```{r comment1, eval=FALSE,echo=FALSE}
+#' The pseudo time calculation could be done much more accurately
+#' We could check the single cell literature
+#'```
+
+#' * Gene of interests
+goi <- read_tsv(here("doc/MA_list_known_SE_genes.txt"),
+                col_types=cols(.default=col_character()))
+
+#' # Functions
+"line_plot" <- function(vsd=vsd,gene_id=gene_id,gene_name=gene_name){
+  message(paste("Plotting",gene_id))
+  sel <- grepl(gene_id,rownames(vsd))
+  stopifnot(sum(sel)==1)
+  
+  p <- ggplot(bind_cols(as.data.frame(colData(vsd)),
+                        data.frame(value=assay(vsd)[sel,])),
+              aes(x=PseudoTime,y=value,col=Tissue,group=Tissue)) +
+    geom_point() + geom_smooth() +
+    scale_y_continuous(name="VST expression") + 
+    ggtitle(label=paste("Expression for: ",gene_id,"(",gene_name,")"))
+  
+  suppressMessages(suppressWarnings(plot(p)))
+  return(NULL)
+}
+
+#' # Plots
+dev.null <- apply(goi,1,function(ro){line_plot(vsd,ro[2],ro[1])})
+
+#' # Differential expression
+#' 
+#' ```{r comment2, eval=FALSE,echo=FALSE}
+#' # limma 
+#' # check https://www.bioconductor.org/packages//2.11/bioc/vignettes/limma/inst/doc/usersguide.pdf
+#' # e.g. chapter 8.5.4
+#' ```
+#' We assume the model to be expression as a function of Stage and Tissue and their interaction
+#' _i.e._ the two variables are not considered independent. However, we will group the Stages, to compare
+#' Maturation (SE B3-5 and ZE B3-6) and Desiccation (SE B6 and ZE B7-9). 
+ 
+vsd$Stage<-ifelse(vsd$PseudoTime<7,"Maturation","Dessication")
+Stage <- vsd$Stage
+Tissue<-vsd$Tissue
+design <- model.matrix(~Stage*Tissue)
+
+#' remove genes with t0o little variance
+mat <- assay(vsd)[rowMads(assay(vsd))>0,]
+
+fit <- lmFit(mat, design)
+fit <- eBayes(fit)
+
+#' The possible coefficients
+colnames(design)
+
+#' ## ZE vs. SE (maturation)
+fit2 <- contrasts.fit(fit, c(0,0,0,0,-1,1))
+fit2 <- eBayes(fit2)
+ZSM <- topTable(fit2,p.value=0.01,lfc=0.5,adjust.method="BH",n=nrow(vsd))
+sample.sel <- vsd$Stage == "Maturation" & vsd$Tissue != "FMG"
+heatmap.2(t(scale(t(mat[rownames(ZSM),sample.sel]))),
+          distfun=pearson.dist,
+          hclustfun=function(X){hclust(X,method="ward.D2")},
+          labRow = NA,trace = "none",
+          labCol = paste(vsd$Tissue,vsd$Time)[sample.sel],
+          col=hpal,cexCol=0.8)
+
+#' ## ZE vs. SE (desiccation)
+fit2 <- contrasts.fit(fit, c(0,0,-1,1,0,0))
+fit2 <- eBayes(fit2)
+ZSD <- topTable(fit2,p.value=0.01,lfc=0.5,adjust.method="BH",n=nrow(vsd))
+sample.sel <- vsd$Stage != "Maturation" & vsd$Tissue != "FMG"
+heatmap.2(t(scale(t(mat[rownames(ZSD),sample.sel]))),
+          distfun=pearson.dist,
+          hclustfun=function(X){hclust(X,method="ward.D2")},
+          labRow = NA,trace = "none",
+          labCol = paste(vsd$Tissue,vsd$Time)[sample.sel],
+          col=hpal,cexCol=0.8)
+
+#' ## Overlap
+grid.newpage()
+grid.draw(venn.diagram(list(ZSD=rownames(ZSD),
+                            ZSM=rownames(ZSM)),NULL,fill=pal[1:2]))
+
+#' # Gene Ontology Enrichment
+GO <- lapply(lapply(list(rownames(ZSD),rownames(ZSM)),
+                    sub,pattern="\\.1$",replacement=""),
+             gopher,background=sub("\\.1","",rownames(mat)),
+             task=list("go"),alpha=0.01,
+             url="pabies")
+
+#' # Export
+dir.create(here("analysis/limma"),showWarnings=FALSE)  
+write_delim(ZSD,here("analysis/limma/ZE-vs-SE-desiccation_DE-results.tsv"))
+write_delim(ZSM,here("analysis/limma/ZE-vs-SE-maturation_DE-results.tsv"))
+write_delim(GO[[1]]$go,here("analysis/limma/ZE-vs-SE-desiccation_DE_GO-results.tsv"))
+write_delim(GO[[2]]$go,here("analysis/limma/ZE-vs-SE-maturation_DE_GO-results.tsv"))
+write_delim(GO[[1]]$go[,c("id","padj")],here("analysis/limma/ZE-vs-SE-desiccation_DE_GO-for-REVIGO-results.tsv"))
+write_delim(GO[[2]]$go[,c("id","padj")],here("analysis/limma/ZE-vs-SE-maturation_DE_GO-for-REVIGO-results.tsv"))
+
+#' # SessionInfo
+#' ```{r session info, echo=FALSE}
+#' sessionInfo()
+#' ```
--- src/R/batchCorrectedData.R
+++ src/R/batchCorrectedData.R
@@ -6,6 +6,7 @@
 #'  html_document:
 #'    toc: true
 #'    number_sections: true
+#'    code_folding: hide
 #' ---
 #' # Setup
 #' * Libraries
@@ -135,20 +136,72 @@ plot(as.hclust(hm$colDendrogram),xlab="",sub="",labels=conds,cex=0.8)
 dir.create(here("analysis/batchCorrection"),showWarnings=FALSE)
 save(vsd,file=here("analysis/batchCorrection/vsd.rda"))
 
-#' # Example limma design
-#' look https://www.bioconductor.org/packages//2.11/bioc/vignettes/limma/inst/doc/usersguide.pdf
-#' e.g. chapter 8.5.4
-#' select the data to drop Stage B0, B9 and B10 (insufficient / lack of replication)
-vsd <- vsd[,!vsd$Time %in% c("B0","B9","B10")]
-vsd$Time <- droplevels(vsd$Time)
-Stage<-vsd$Time
-Tissue<-vsd$Tissue
-design <- model.matrix(~Stage*Tissue)
-fit <- lmFit(assay(vsd), design)
-fit <- eBayes(fit)
-colnames(design)
-topTable(fit, coef=10-9)
+#' # Data sub-selection
+#' Based on the visualisation above, we focus on ZE Stage 3-9 and SE Stage 3-6
+sample.sel <- (vsd$Time %in% paste0("B",3:6) & vsd$Tissue =="SE") | 
+  (vsd$Tissue=="ZE" & vsd$Time %in% paste0("B",3:9))
 
+vsd <- vsd[,sample.sel]
+vsd$Time <- droplevels(vsd$Time)
+
+#' ## PCA
+pc <- prcomp(t(assay(vsd)))
+percent <- round(summary(pc)$importance[2,]*100)
+
+#' * Cumulative components effect
+#' 
+#' We define the number of variable of the model
+nvar=2
+
+#' An the number of possible combinations
+nlevel=nlevels(vsd$Tissue) * nlevels(vsd$Time)
+
+#' We plot the percentage explained by the different components, the
+#' red line represent the number of variable in the model, the orange line
+#' the number of variable combinations.
+ggplot(tibble(x=1:length(percent),y=cumsum(percent)),aes(x=x,y=y)) +
+  geom_line() + scale_y_continuous("variance explained (%)",limits=c(0,100)) +
+  scale_x_continuous("Principal component") + 
+  geom_vline(xintercept=nvar,colour="red",linetype="dashed",size=0.5) + 
+  geom_hline(yintercept=cumsum(percent)[nvar],colour="red",linetype="dashed",size=0.5) +
+  geom_vline(xintercept=nlevel,colour="orange",linetype="dashed",size=0.5) + 
+  geom_hline(yintercept=cumsum(percent)[nlevel],colour="orange",linetype="dashed",size=0.5)
+
+#' ### 2D
+pc.dat <- bind_cols(PC1=pc$x[,1],
+                    PC2=pc$x[,2],
+                    as.data.frame(colData(vsd)))
+
+p <- ggplot(pc.dat,aes(x=PC1,y=PC2,col=Time,shape=Tissue,text=NGI.ID)) + 
+  geom_point(size=2) + 
+  ggtitle("Principal Component Analysis",subtitle="variance stabilized counts")
+
+ggplotly(p) %>% 
+  layout(xaxis=list(title=paste("PC1 (",percent[1],"%)",sep="")),
+         yaxis=list(title=paste("PC2 (",percent[2],"%)",sep="")))
+
+#' ## Hierarchical clustering
+conds <- factor(paste(vsd$Time,vsd$Tissue))
+sels <- rangeFeatureSelect(counts=assay(vsd),
+                           conditions=conds,
+                           nrep=3)
+
+
+vst.cutoff <- 4
+
+hc <- hclust(pearson.dist(scale(t(assay(vsd)[sels[[vst.cutoff+1]],]))),
+       method="ward.D2")
+
+plot(hc,xlab="",sub="",labels=conds,cex=0.8)
+
+
+#' # Conclusion
+#' The batch correction worked very nicely and while the "Tissue" Se vs. ZE still explains 
+#' the variance in the first principal component, the second component explains the developmental
+#' series and is in agreement with the expectations, especially with regards to the maturation (SE, B3,4,5, ZE B3-B6)
+#' and desiccation (SE, B6 - ZE B7-9) time points.
+#' ```{r mock,eval=FALSE,echo=FALSE}
+#' ```
 #' # SessionInfo
 #' ```{r session info, echo=FALSE}
 #' sessionInfo()
